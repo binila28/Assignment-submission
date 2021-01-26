e0dfcdafa7a32847c309654d00739dac8c243e3c
binila28 <77821667+binila28@users.noreply.github.com>
2021-01-26 12:46:34 +0530

    DAY4 ASSIGNMENT SUBMITTED

---
+++ DAY4/addnum.js
@@ -0,0 +1,14 @@
+
+
+function additions(){
+    var input1=parseInt(document.getElementById("first number").value);
+    var input2=parseInt(document.getElementById("second number").value);
+    console.log(input1);
+    console.log(input2);
+
+var c=input1+input2;
+
+
+document.getElementById("answer").value = c;
+console.log(c);
+}
